6991649a7c9402c61c1fe9fe35168313dcb534ba
Johann Franz H <johanfranz18@gmail.com>
2021-08-06 03:34:42 -0500

    Fase3 - Deteccion de plagio

---
+++ Fase3/file1.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+
+vector<float> allCosines;
+
+typedef map<string, int> DocMap;
+typedef map<string, int> QueryMap;
+typedef map<string, float> Similarity;
+
+void countWords(istream& in, DocMap& words) {
+    string s;
+    while (in >> s) {
+        ++words[s];
+     }
+  }
+
+
+int dotprod(int x, int y)
+{ return x*y;
+}
+
+int findTheDot(QueryMap q_obj, DocMap w)
+{ int dot=0;
+  for (QueryMap::iterator l=q_obj.begin(); l != q_obj.end(); ++l)
+	for (DocMap::iterator p = w.begin(); p != w.end(); ++p)
+		if (l->first == p->first)
+			 dot+=dotprod(l->second, p->second);
+  return dot;
+}
+
+float Q_mod(QueryMap q_obj)
+{ float store=0;
+  for (QueryMap::iterator l=q_obj.begin(); l != q_obj.end(); ++l)
+	store+=(l->second*l->second);
+  return sqrt(store);
+}
